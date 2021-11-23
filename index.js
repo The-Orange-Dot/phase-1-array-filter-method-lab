99b8f3c92dedc9296c3aefcb1bfdee773c806449
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-24 08:01:21 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-// Code your solution here
-
-const drivers = [
-  {
-    name: 'Bobby',
-    hometown: 'Pittsburgh' },
-  {
-    name: 'Sammy',
-    hometown: 'New York' } ,
-  {
-    name: 'Sally',
-    hometown: 'Cleveland' },
-  {
-    name: 'Annette',
-    hometown: 'Los Angeles' },
-  {
-    name: 'Bobby',
-    hometown: 'Tampa Bay' }
-];
-
-
-
-
 function findMatching(db, names) {
   const result = db.filter(db => db.toLowerCase() == names.toLowerCase())
   return result
 }
-
-
 
 function fuzzyMatch (db, letters) {
   let collection = []
@@ -37,6 +12,8 @@ function fuzzyMatch (db, letters) {
   return collection
 }
 
+fuzzyMatch(drivers, "Sa")
+
 function matchName(db, n) {
   let collection = []
   for (let item of db) {
@@ -46,5 +23,3 @@ function matchName(db, n) {
   }
   return collection
 }
-
-matchName(drivers, "Bobby")
