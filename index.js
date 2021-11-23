442eee3abcc70af548aa9283746cf3fc88481d38
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-24 07:50:30 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+
+
+
+function findMatching(db, names) {
+  const result = db.filter(db => db.toLowerCase() == names.toLowerCase())
+  return result
+}
+
+
+
+function fuzzyMatch (db, letters) {
+  let collection = []
+  for (let item of db) {
+    if (!item.indexOf(letters))
+     collection.push(item)
+  }
+  return collection
+}
+
+function matchName(db, n) {
+  let collection = []
+  for (let item of db) {
+    if (item.name == n) {
+      collection.push(item)
+    }
+  }
+  return collection
+}
+
+matchName(drivers, "Bobby")
